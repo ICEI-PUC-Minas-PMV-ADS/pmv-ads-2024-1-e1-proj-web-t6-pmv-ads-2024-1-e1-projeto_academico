5c9f4392524bc0c3375454814d07b0d8d064b193
kevinsant13 <1522010@sga.pucminas.br>
2024-06-05 15:32:14 -0300

    Funcionalidade Barra de Pesquisa Lista de Turmas / Lançar Notas

--- codigo-fonte/src/pages/turmas/notas_lista/index.js
+++ codigo-fonte/src/pages/turmas/notas_lista/index.js
@@ -4,5 +4,24 @@ import './index.css';
 export default {
     init() {
         console.log('turma notas');
-    }
+        document.getElementById('searchButton').addEventListener('click', function() {
+            const query = document.getElementById('searchInput').value.toLowerCase();
+            filterTable(query);
+        });
+        
+        function filterTable(query) {
+            const tableRows = document.querySelectorAll('.tableValues tr');
+        
+            tableRows.forEach(function(row) {
+                const rowData = Array.from(row.children).map(cell => cell.textContent.toLowerCase());
+                const rowMatchesQuery = rowData.some(data => data.includes(query));
+                
+                if (rowMatchesQuery) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+            
+        }    }
 }
